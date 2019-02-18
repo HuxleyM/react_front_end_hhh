b600cde4bd44008783d37e89eab63ecd6f513448
Huxley_Millard <gerogiamillardccm@gmail.com>
2019-02-18 15:48:23 +0000

    setting up enzyme

--- src/App.js
+++ src/App.js
@@ -1,1 +1,14 @@
-// export default 
+
+import React, {Component} from 'react';
+
+export default class App extends Component {
+    render() {
+        return (
+            <div className="App">
+                <h1>Hello, React!</h1>
+            </div>
+        );
+    }
+}
+
+
--- src/index.js
+++ src/index.js
@@ -1,17 +1,8 @@
 import React,{ Component } from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
-//import App from './App';
+import App from './App';
 
-class App extends Component {
-    render() {
-        return (
-            <div className="App">
-                <h1>Hello, React!</h1>
-            </div>
-        );
-    }
-}
 
 
 
