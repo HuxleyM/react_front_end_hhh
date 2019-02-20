02d4ff8b4cd0221d6b1601a35faa382d2a1ec651
Jethro Whaley <jethro.whaley@gmail.com>
2019-02-20 12:54:08 +0000

    tests - updating for the swap

--- src/test/App.test.js
+++ src/test/App.test.js
@@ -3,11 +3,14 @@ import React, {Component} from 'react';
 import {shallow, mount, render} from 'enzyme'
 
 
-describe('App js shouls say hello world', ()=>{
+describe('App js shouls say Keep Ahead', ()=>{
+    let renderWrapper
+    let wrapper
 
-    const renderWrapper = render(< App/>)
-    const wrapper = shallow(< App/>)
-
+    beforeEach(() => {
+      renderWrapper = render(< App/>)
+      wrapper = shallow(< App/>)
+    })
 
     it('should display the app name', ()=>{
         expect(renderWrapper.find('div h1').text()).toBe('Keep Ahead')
@@ -23,7 +26,7 @@ describe('App js shouls say hello world', ()=>{
 
     describe('Show Venues', () => {
       it('renders a list of venues', ()=> {
-        expect(renderWrapper.find('ul').text()).toContain("crisis cafe")
+        expect(renderWrapper.find('li').text()).toEqual("Crisis Cafe")
       })
     })
 })
