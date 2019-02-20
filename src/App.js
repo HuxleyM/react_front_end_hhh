c84b736f98f18351b3072084e0579244b7f8279b
stonefarmer9 <g.p.levaillant@gmail.com>
2019-02-20 11:39:29 +0000

    data passing to components

--- src/App.js
+++ src/App.js
@@ -1,64 +1,39 @@
 
 import React, {Component} from 'react';
 import ShowVenues from './components/showvenues'
-import DisplayVenue from './components/displayVenue'
+//import DisplayVenue from './components/displayVenue'
 
 export default class App extends Component {
-  constructor() {
-    super()
+  constructor(props) {
+    super(props)
+    console.log('line 9')
     this.state = {
-      venues: [
-        {
-          'name': 'crisis cafe',
-          'address': '58 commercial street, London, E1 ABC'
-        },
-        {
-          'name': 'starbucks',
-          'address': '58 commercial road, London, E2 BBC'
-        }
-      ]
+      venues: []
+      }
     }
-    this.props = this.state;
-  }
 
-  // componentDidMount() {
-  //
-  //   fetch('https://enigmatic-badlands-83570.herokuapp.com/api/v1/venues')
-  //   .then((response) => {
-  //     console.log(response);
-  //     return response.json()
-  //   })
-  //   .then((data) => {
-  //     // this.setState({
-  //     //   venues: data
-  //     // })
-  //     console.log(JSON.stringify(data));
-  //   })
-  // }
+   componentDidMount() {
+     const that = this;
+     fetch('https://enigmatic-badlands-83570.herokuapp.com/api/v1/venues')
+    .then((response) => {
+      return response.json()
+    })
+    .then((response) => {
+      that.setState({
+        venues: response
+      })
+    })
+  }
     render() {
-      const venueList = this.state;
+      console.log(`hi line 31 ${this.state.venues}`)
         return (
+
             <div className="App">
                 <h1>Keep Ahead</h1>
-                <ShowVenues venueList={venueList}/>
+                <ShowVenues
+                venueList={this.state.venues}
+                />
             </div>
         );
     }
 }
-
-
-// {
-//       method: 'POST',
-//       headers: {
-//         'Content-Type': 'application/json'
-//       },
-//       body: body
-//     }
-
-
-// headers: {
-//     'Access-Control-Allow-Credentials' : true,
-//     'Access-Control-Allow-Origin':'*',
-//     'Access-Control-Allow-Methods':'GET',
-//     'Access-Control-Allow-Headers':'application/json',
-//   }
--- src/components/showvenues.js
+++ src/components/showvenues.js
@@ -3,29 +3,27 @@ import React, {Component} from 'react';
 export default class ShowVenues extends Component {
   constructor (props){
     super(props);
-    console.log(props);
-    // this.list = this.requestVenueList();
+    console.log(props)
+    //console.dir(`Print Beta:${props}`);
   }
-
-  // requestVenueList() {
-  //   fetch('https://enigmatic-badlands-83570.herokuapp.com/api/v1/venues')
-  //   .then((response) => {return response.json()})
-  //   .then((response) => {console.log(response)})
-  // }
 
   showVenue() {
     console.log('hi');
   }
 
   render() {
-    const venues = this.props.venues;
-    const list = Object.entries(venues);
-    // list.forEach(venues)((venue, index) => {
-      return( null
-        // <p key={index}><li>{venue.name} {venue.address}</li>
-        // <button onClick={this.showVenue}>view</button></p>
-      )
-    // })
-    return <ul>{list}</ul>
+
+    return <div>Hello world</div>
   }
 }
+
+
+// const venues = this.props.venueList;
+// const list = Object.entries(venues);
+// console.log(`Print Alpha:${list}`)
+// // list.forEach(venues)((venue, index) => {
+//   //return( null
+//     // <p key={index}><li>{venue.name} {venue.address}</li>
+//     // <button onClick={this.showVenue}>view</button></p>
+//   //)
+// // })
