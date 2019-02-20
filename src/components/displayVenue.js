6e1fe3ce0701f0a1f6ac50cd60cf0d9b6075737b
stonefarmer9 <g.p.levaillant@gmail.com>
2019-02-20 16:23:05 +0000

    Feat: Venue ID persists to the displayVenue component

--- src/components/displayVenue.js
+++ src/components/displayVenue.js
@@ -3,10 +3,15 @@ import React, {Component} from 'react';
 export default class DisplayVenue extends Component {
   constructor(props) {
     super(props);
+    this.state = {
+      venues: this.props.venueList,
+      venueID: this.props.id
+    }
+    console.log(this.props)
+    console.log(this.state)
   }
 
 render() {
-   console.log('1',this.props)
    return(
      <div>
      <h1>Hello world</h1>
--- src/components/showvenues.js
+++ src/components/showvenues.js
@@ -7,35 +7,43 @@ export default class ShowVenues extends Component {
     this.state = {
       venues : this.props.venueList,
       showVenue : false,
+      venueID: -1
     };
     this._onButtonClick = this._onButtonClick.bind(this);
   }
 
-
-
   _onButtonClick() {
     this.setState({
-      showVenue: true,
+      showVenue: true
     });
   }
 
+  _updateIndex(index){
+    console.log("Index print",index)
+    this.setState({
+      venueID: index
+    })
+  }
+
+//onclick={()=>{ f1(); f2() }}
   render() {
     const venues = this.state.venues;
+    const id = this.state.venueID
 
     const list = venues.map((venue, index) => {
       return (
         <p key={index}>
           <li>{venue.name} {venue.address}</li>
-
-          <button onClick={this._onButtonClick}>view</button>
-
+          <button onClick={()=>{this._onButtonClick(); this._updateIndex(index)}}>view</button>
         </p>
       )
-    }) 
+    })
     return (<div>
-    { this.state.showVenue ? 
-     <DisplayVenue/> :
-      <ul>{list}</ul> 
+    { this.state.showVenue ?
+     <DisplayVenue
+     venueList={venues}
+     venueId={id}/> :
+      <ul>{list}</ul>
     }</ div>)
 
   }
