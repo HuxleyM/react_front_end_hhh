549b8f7aabdc6b52831f12a0d2c4284c0493bef2
Huxley_Millard <gerogiamillardccm@gmail.com>
2019-02-21 20:18:14 +0000

    it ok

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ export default class App extends Component {
     render() {
         return (
             <div className="App">
-                <h1>Hello, world!</h1>
+                <h1>Hello, me!</h1>
             </div>
         );
     }
