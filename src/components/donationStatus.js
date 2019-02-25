2a7a20373f19b3454dbe23c55ea0a8e0ff9e6507
Jethro Whaley <jethro.whaley@gmail.com>
2019-02-25 21:08:28 +0000

    refactor: donation status/redemption extracted to it's own component

---
+++ src/components/donationStatus.js
@@ -0,0 +1,42 @@
+import React, {Component} from 'react';
+
+export default class DonationStatus extends Component {
+  constructor(props) {
+    super(props);
+    this.state={
+      redeemed: false
+    }
+  }
+
+  redeemDonation = () => {
+    const { donation, venue } = this.props;
+    const body = JSON.stringify({ redeemed: true })
+
+    fetch(`http://localhost:3000/api/v1/venues/${venue.id}/donations/${donation.id}`, {
+      method: 'PUT',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: body
+    })
+    .then((response) => {
+      this.setState({
+        redeemed: true
+      })
+    })
+  }
+
+  // https://enigmatic-badlands-83570.herokuapp.com/api/v1/venues/${venueId}/donations
+
+  render() {
+    const { redeemed } = this.state;
+    const { donation } = this.props;
+
+    return !redeemed && (
+        <p>
+          <li>{donation.amount} {donation.passphrase}</li>
+          <button onClick={this.redeemDonation}>Redeem</button>
+        </p>
+    )
+  }
+}
--- src/components/venueProfile.js
+++ src/components/venueProfile.js
@@ -1,4 +1,5 @@
 import React, {Component} from 'react';
+import DonationStatus from './donationStatus';
 
 const venue1 ={
   id: 1,
@@ -31,46 +32,15 @@ export default class VenueProfile extends Component {
     })
   }
 
-  // https://enigmatic-badlands-83570.herokuapp.com/api/v1/venues/${venueId}/donations
-
-  redeemDonation = id => {
-
-    const body = JSON.stringify({ redeemed: true })
-
-    const venueId = this.state.venue.id;
-    const donationId = id;
-
-    fetch(`http://localhost:3000/api/v1/venues/${venueId}/donations/${donationId}`, {
-      method: 'PUT',
-      headers: {
-        'Content-Type': 'application/json'
-      },
-      body: body
-    })
-    .then((response) => {
-      return response.json()
-    })
-
-  }
-
   render() {
     const { venue } = this.state;
     const { donations } = this.state;
     const openDonations = donations.filter(donation => donation.redeemed !== true)
 
-    const list = openDonations.map((donation) => {
-      return(
-        <div className="donationList">
-          <p key={donation.id}>
-            <li>{donation.amount} {donation.passphrase}</li>
-            <button onClick={()=> {this.redeemDonation(donation.id)}}>Redeem</button>
-          </p>
-        </div>
-      )
-    })
+    const list = openDonations.map(donation => <DonationStatus donation={donation} venue={venue} />)
 
     return(
-      <div>
+      <div className="donationList">
         <h1>{venue.name}</h1>
         <ul>{list}</ul>
       </div>
