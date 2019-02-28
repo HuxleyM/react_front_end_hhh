e4d7ecf3ab1b29b0f90ccbb91dabe2610063cba8
Huxley_Millard <gerogiamillardccm@gmail.com>
2019-02-28 23:19:41 +0000

    refactor and styling

--- src/App.js
+++ src/App.js
@@ -56,19 +56,19 @@ export default class App extends Component {
 
     const { venues, venuePortal } = this.state;
       return (
-        <div>
-        {!venuePortal &&  <button id='venue_portal_button' onClick={()=> { this.venuePortal() } }>Venue Portal</button>}
-        <h1 id='title'>Keep Ahead</h1>
-        <hr/>
+        <div className='wrapper'>
+          {!venuePortal &&  <button id='venue_portal_button' onClick={()=> { this.venuePortal() } }>Venue Portal</button>}
+          <h1 id='title'>Keep Ahead</h1>
+          <hr/>
 
-        {
-          (!venuePortal && venues.length > 0) ?
-          <ShowVenues
-          venues={venues}
-          />
-          :
-          <VenuePortal />
-        }
+          {
+            (!venuePortal && venues.length > 0) ?
+            <ShowVenues
+            venues={venues}
+            />
+            :
+            <VenuePortal />
+          }
         </div>
       );
     }
--- src/components/venuePortal.js
+++ src/components/venuePortal.js
@@ -37,20 +37,6 @@ export default class VenuePortal extends Component {
 
       const { venue, loggedIn } = this.state;
 
-      const loginButtons = (
-        <div id="login_buttons">
-            <button id='sign_up_button' onClick={()=> { this._changeProcess('signup') } }>Sign Up</button>
-            <button id='sign_in_button'onClick={()=> { this._changeProcess('signin') } }>Sign In</button>
-
-            { this.state.process == 'signup' &&
-            < SignUp action={this.handler} /> }
-          
-            {this.state.process == 'signin' &&
-            < SignIn action={this.handler} /> }
-      
-      </div> )
-      
-    
       return (
         <div>
           {loggedIn ? 
@@ -59,9 +45,17 @@ export default class VenuePortal extends Component {
               <VenueProfile venue={venue} />
             </div>
             :
-            <div>
-              {loginButtons}
+            
+            <div id="login_buttons">
               <button onClick={() => {window.location = '/'}}>Home</button>
+              <button id='sign_up_button' onClick={()=> { this._changeProcess('signup') } }>Sign Up</button>
+              <button id='sign_in_button'onClick={()=> { this._changeProcess('signin') } }>Sign In</button>
+
+              { this.state.process == 'signup' &&
+              < SignUp action={this.handler} /> }
+            
+              {this.state.process == 'signin' &&
+              < SignIn action={this.handler} /> }
             </div>
           }
         </div>
