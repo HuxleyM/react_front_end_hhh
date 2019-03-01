35c44774167d78b312bdad113c7027c402cc1c91
Joao Abbott-Gribben <joao.abbott.gribben@gmail.com>
2019-03-01 14:00:17 +0000

    TWEAKED CHANGES

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
 
 import React, {Component} from 'react';
-import ShowVenues from './components/showvenues'
-import VenuePortal from './components/venuePortal'
+import ShowVenues from './components/showvenues';
+import VenuePortal from './components/venuePortal';
+// import Logo from './keep_ahead_logo.png';
 
 
 export default class App extends Component {
@@ -52,13 +53,17 @@ export default class App extends Component {
     })
   }
 
+
   render() {
 
     const { venues, venuePortal } = this.state;
       return (
+        
         <div className='wrapper'>
+        
           {!venuePortal &&  <button id='venue_portal_button' onClick={()=> { this.venuePortal() } }>Venue Portal</button>}
-          <h1 id='title'>Keep Ahead</h1>
+          <h1 id='title'><img id="logo" src={require("./keep_ahead_logo.png")} />Keep Ahead</h1>
+          
           <hr/>
 
           {
