08dd0d3ce9dec6005191c757b26f8c9726305b51
Jethro Whaley <jethro.whaley@gmail.com>
2019-03-01 14:09:50 +0000

    fix: tweaks

--- src/App.js
+++ src/App.js
@@ -56,12 +56,12 @@ export default class App extends Component {
   render() {
     const { venues, venuePortal } = this.state;
       return (
-        
+
         <div className='wrapper'>
-        
+
           {!venuePortal &&  <button id='venue_portal_button' onClick={()=> { this.venuePortal() } }>Venue Portal</button>}
           <h1 id='title'><img id="logo" src={require("./keep_ahead_logo.png")} />Keep Ahead</h1>
-          
+
           <hr/>
 
           {
@@ -70,7 +70,7 @@ export default class App extends Component {
             venues={venues}
             />
             :
-            <VenuePortal />
+             (venuePortal) ? <VenuePortal /> : <h2>Loading...</h2>
           }
         </div>
       );
